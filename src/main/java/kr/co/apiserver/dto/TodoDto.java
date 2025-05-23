9eb49195c56a12b384a862e1b70b8b2cd52199ef
parkhg09 <parkhg09@gmail.com>
2025-05-23 23:59:02 +0900

    TodoService, TodoDto

---
+++ src/main/java/kr/co/apiserver/dto/TodoDto.java
@@ -0,0 +1,20 @@
+package kr.co.apiserver.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDto {
+
+    private Long tno;
+    private String title;
+    private String content;
+    private boolean complete;
+    private LocalDate dueDate;
+
+}
