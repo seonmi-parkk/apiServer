d599c5dfa36a8dcf2e3b6ee679cc9d9483b05db8
parkhg09 <parkhg09@gmail.com>
2025-05-24 21:57:02 +0900

    todo 생성, 수정, 삭제, 리스트 기능 구현 및 테스트

--- src/main/java/kr/co/apiserver/service/TodoServiceImpl.java
+++ src/main/java/kr/co/apiserver/service/TodoServiceImpl.java
@@ -2,17 +2,21 @@ package kr.co.apiserver.service;
 
 import jakarta.transaction.Transactional;
 import kr.co.apiserver.domain.Todo;
+import kr.co.apiserver.dto.PageRequestDto;
+import kr.co.apiserver.dto.PageResponseDto;
 import kr.co.apiserver.dto.TodoDto;
 import kr.co.apiserver.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
 
@@ -26,5 +30,74 @@ public class TodoServiceImpl implements TodoService {
 
         return entityToDto(todo);
     }
+
+    @Override
+    public Long register(TodoDto dto) {
+
+        Todo todo = dtoToEntity(dto);
+
+        Todo result = todoRepository.save(todo);
+        return result.getTno();
+    }
+
+    @Transactional
+    @Override
+    public void modify(TodoDto dto) {
+        Optional<Todo> result = todoRepository.findById(dto.getTno());
+
+        Todo todo = result.orElseThrow();
+
+        todo.setTitle(dto.getTitle());
+        todo.setContent(dto.getContent());
+        todo.setComplete(dto.isComplete());
+        todo.setDueDate(dto.getDueDate());
+
+    }
+
+    @Override
+    public void remove(Long tno) {
+        todoRepository.deleteById(tno);
+    }
+
+    @Override
+    public PageResponseDto<TodoDto> getList(PageRequestDto pageRequestDto) {
+        log.info("getPage : "+pageRequestDto.getPage());
+        log.info("getSize : "+pageRequestDto.getSize());
+        // JPA
+        Page<TodoDto> dtoList  = todoRepository.search1(pageRequestDto, pageRequestDto.toPageable());
+
+        PageResponseDto<TodoDto> responseDto =
+                PageResponseDto.<TodoDto>withAll()
+                        .dtoList(dtoList.getContent())
+                        .pageRequestDto(pageRequestDto)
+                        .totalElements(dtoList.getTotalElements())
+                        .totalPage(dtoList.getTotalPages())
+                        .currentPage(dtoList.getNumber())
+                        .isLast(dtoList.isLast())
+                        .isFirst(dtoList.isFirst())
+                        .build();
+
+        return responseDto;
+    }
+
+//    @Override
+//    public PageResponseDto<TodoDto> getList(PageRequestDto pageRequestDto) {
+//        log.info("getPage : "+pageRequestDto.getPage());
+//        log.info("getSize : "+pageRequestDto.getSize());
+//        // JPA
+//        Page<Todo> result = todoRepository.search1(pageRequestDto);
+//
+//        List<TodoDto> dtoList = result.get()
+//                .map(todo -> entityToDto(todo)).collect(Collectors.toList());
+//
+//        PageResponseDto<TodoDto> responseDto =
+//                PageResponseDto.<TodoDto>withAll()
+//                        .dtoList(dtoList)
+//                        .pageRequestDto(pageRequestDto)
+//                        .total(result.getTotalElements())
+//                        .build();
+//
+//        return responseDto;
+//    }
 
 }
